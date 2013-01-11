a5f499c5279f9de647072fc4b982b8ffac431dac
Dr Blinken <drblinken@gmail.com>
2013-01-11 03:04:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetpipelineTryout::Application.config.secret_token = '98804483083bc4c5e4152ec3fc9ce9d48ba9b1c01c62b2d7a53cce1c24e0abc8ec633a38d71aaa7a831fed6304d40c42724aba8a5c555011202bdb32a28d630c'
